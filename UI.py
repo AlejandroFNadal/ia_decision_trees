0b52ed4953c300da741675d6563d58a7bb989209
ExequielSchuster <exe97as@gmail.com>
2021-10-06 12:19:46 -0300

    UI: adding threshold slider and changin delimiter selector

--- UI.py
+++ UI.py
@@ -5,33 +5,24 @@ import numpy as np
 #pip install openpyxl
 #pip install xlrd
 
-uploaded_file = st.file_uploader("Choose a file (CSV, Text or Excel File)",  accept_multiple_files=False, type = ['csv','txt','xlsx'])
+uploaded_file = st.file_uploader("Ingrese un archivo (CSV, Text o Excel File):",  accept_multiple_files=False, type = ['csv','txt','xlsx'])
+delimiter = st.radio("Seleccione un delimitador:",(',', ';', '-'))
+threshold = st.slider('Seleccione el valor de Threshold a usar:', 0.0, 1.0, 0.1, 0.05)  # Minimo, Maximo, Default, Step
 button = st.button('Cargar Archivo')
 
 if button == True:
     if uploaded_file is not None:
         st.write("Resumen de los datos:")
-        if (uploaded_file.name).split('.')[1] == 'csv': # Aca usamos la funcion de acuerdo al tipo de archivo
-            dataframe = pd.read_csv(uploaded_file)
+        if (uploaded_file.name).split('.')[1] in ['csv','txt']:     # Aca usamos la funcion de acuerdo al tipo de archivo 
+            dataframe = pd.read_csv(uploaded_file, sep=delimiter)   # CSV y TXT
             st.write(dataframe.head())
 
         elif (uploaded_file.name).split('.')[1] == 'xlsx':
-            dataframe = pd.read_excel(uploaded_file, index_col=0,  engine='openpyxl')
+            dataframe = pd.read_excel(uploaded_file, engine='openpyxl')     # EXCEL
             st.write(dataframe.head())
 
-        elif (uploaded_file.name).split('.')[1] == 'txt':
-            delimiter = st.text_input('Delimitador', max_chars=1, autocomplete=',')
-            button = st.button('Cargar Delimitador')
-            if button == True:
-                if delimiter != '':
-                    st.write(delimiter)
-                    dataframe = pd.read_csv('output_list.txt', sep=delimiter, header=None)
-                    st.write(dataframe.head())
-                else:
-                    st.write('Por favor, inserte un delimitador')
-
     elif uploaded_file is None:
-        st.write('Cargue un Archivo')
+        st.write('Por favor, ingrese un Archivo.')
 
         
     
