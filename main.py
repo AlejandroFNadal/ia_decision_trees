ebe11cab16e3cae3cc2f3c752d1ef98c37557ab1
FacuCuzziol <facucuzziol@gmail.com>
2021-10-08 04:48:21 +0100

    adding main .py file to call utils classes and methods

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import graphviz
+from utils.setCases.setCases import SetCases
+from utils.node.Node import Node
+from utils.decisionTree.DecisionTree import decisionTree
+from utils.predictions.Predictions import predict_cases
+from config.config import name_counter
+
+
+df = pd.read_csv('data/automoviles.csv').head(10)
+target = 'AltaGama'
+#graph = graphviz.Digraph()
+classValues = list(df[target].value_counts().index) 
+
+set_init = SetCases(df,len(df),target,classValues)
+root_node = Node(set_init, 0)
+decisionTree(set_init, set_init.get_attributes(),root_node,0,classValues)
+
+df['test_result'] = predict_cases(df,root_node)
+print(df[[target,'test_result']])
