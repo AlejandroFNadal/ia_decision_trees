2588bbb5d42892a596c1b51694eab76173b6f4ed
FacuCuzziol <facucuzziol@gmail.com>
2021-10-11 17:57:24 -0300

    Update UI

--- UI.py
+++ UI.py
@@ -4,6 +4,17 @@ import pandas as pd
 import numpy as np
 #pip install openpyxl
 #pip install xlrd
+import pandas as pd
+import graphviz
+from src.setCases.setCases import SetCases
+from src.node.Node import Node
+from src.decisionTree.DecisionTree import decisionTree
+from src.predictions.Predictions import predict_cases
+from config.config import name_counter,graph_array
+
+def _update_slider(value):
+    st.session_state["test_slider"] = value
+    st.graphviz_chart(graph_array[value])
 
 uploaded_file = st.file_uploader("Ingrese un archivo (CSV, Text o Excel File):",  accept_multiple_files=False, type = ['csv','txt','xlsx'])
 delimiter = st.radio("Seleccione un delimitador:",(',', ';', '-'))
@@ -20,9 +31,30 @@ if button == True:
         elif (uploaded_file.name).split('.')[1] == 'xlsx':
             dataframe = pd.read_excel(uploaded_file, engine='openpyxl')     # EXCEL
             st.write(dataframe.head())
+            
+        target = 'class'
+        graph = graphviz.Digraph()
+        classValues = list(dataframe[target].value_counts().index) 
+
+        set_init = SetCases(dataframe,len(dataframe),target,classValues)
+        root_node = Node(set_init, 0)
+        decisionTree(set_init, set_init.get_attributes(),root_node,0,classValues)
+        root_node.printTree(0, graph, root_node,name_counter)
+        
+        if "test_slider" not in st.session_state:
+            st.session_state["test_slider"] = -1
+        
+        graphs = st.slider('Seleccione el paso de la ejecucion para graficar:', 0, len(graph_array), 0, 1,key="test_slider")
+        st.button("Update slider values", on_click=_update_slider, kwargs={"value": graphs})
+        #if st.session_state["test_slider"] > 0:
+        #    st.graphviz_chart(graph_array[graphs])
+        #for graph in graph_array:  
+        #    st.graphviz_chart(graph)
 
     elif uploaded_file is None:
         st.write('Por favor, ingrese un Archivo.')
+        
+    
 
         
     
