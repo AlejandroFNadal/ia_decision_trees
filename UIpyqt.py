c5afd313745a89f3d3fe4572b368dc28e7ef9f1d
ExequielSchuster <exe97as@gmail.com>
2021-10-17 23:03:55 -0300

    Progressing with PYQT inteface

--- UIpyqt.py
+++ UIpyqt.py
@@ -1,36 +1,76 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
-from PyQt5.QtCore import QSize    
-from PyQt5.QtGui import QIcon
+import pandas as pd
+from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtCore import Qt
+from main import MainFunction
 
-
-
-class App(QWidget):
+class App(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title = 'Intentando'
-        self.left = 100
-        self.top = 100
-        self.width = 640
-        self.height = 480
-        self.initUI()
+        uic.loadUi('./UI.ui',self)
+        self.setWindowTitle('Arbol de Decision - Grupo 7')
+        self.fileName = ''
+        self.df = ''
+        self.fileNameText.setText("Nombre del archivo:")
+        self.generarArbolButton.setEnabled(False)
+        self.cargarArchivoButton.clicked.connect(self.openFile) #Una vez cargado el archivo, se habilita el boton de generar el arbol
+        self.generarArbolButton.clicked.connect(self.executeMainFunction)
     
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.openFileNameDialog()
-        self.show()
-    
-    def importFile(self):
+    def openFile(self):
         options = QFileDialog.Options()
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*)", options=options)
-        if fileName:
-            print(fileName)
-
+        file = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","Files (*.txt *.csv *.xlsx);;All Files (*)", options=options)
+        if file:
+            self.fileName, _ = file
+            print("MIRA ACA",self.separatorSelector.currentText())
+            self.fileNameText.setText("Nombre del archivo: " + (self.fileName).split('/')[-1]) # Muestra el nombre del archivo cargado
+            self.generarArbolButton.setEnabled(True)
+        else:
+            print("error")
     
+    def executeMainFunction(self):
+        if (self.fileName).split('.')[-1] in ['csv','txt']:     # Aca usamos la funcion de acuerdo al tipo de archivo 
+                self.df = pd.read_csv(self.fileName, sep=(self.separatorSelector.currentText()))   # CSV y TXT
+        elif (self.fileName).split('.')[-1] == 'xlsx':
+            self.df = pd.read_excel(self.fileName, engine='openpyxl')     # EXCEL
+        print(self.df.head())
+        
+        self.model = TableModel(self.df.head()) # Aca se cargan los datos en el resumen de los datos
+        self.tableView.setModel(self.model)
+
+        #MainFunction(df,self.thresholdSelector.value())
+
+
+class TableModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            value = self._data.iloc[index.row(), index.column()]
+            return str(value)
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
+
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    GUI = App()
+    GUI.show()
     sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -9,48 +9,47 @@ from src.split.split import split_dataset
 from src.train.train import train
 from src.preprocessing.preprocessing import remove_continuous_columns,impute_with_mode
 
+def MainFunction(df,threshold):
 
-df = pd.read_csv('data/chess.csv')
-target = 'class'
-removed_continuous = remove_continuous_columns(df)
-df = removed_continuous[1]
-removed_columns = removed_continuous[0]
+    target = df.columns[-1]
+    removed_continuous = remove_continuous_columns(df)
+    df = removed_continuous[1]
 
-df = impute_with_mode(df)
-print(df.head())
+    df = impute_with_mode(df)
+    print(df.head())
 
-df_train, df_test = split_dataset(df,0.7,target)
-print(df_train.shape)
-print(df_test.shape)
-graph = graphviz.Digraph()
-""" 
-classValues = list(df[target].value_counts().index) 
+    df_train, df_test = split_dataset(df,0.7,target)
+    print(df_train.shape)
+    print(df_test.shape)
+    graph = graphviz.Digraph()
+    """ 
+    classValues = list(df[target].value_counts().index) 
 
-set_init = SetCases(df,len(df),target,classValues)
-root_node = Node(set_init, 0)
-decisionTree(set_init, set_init.get_attributes(),root_node,0,classValues)
-"""
-tree_gain = train(df_train, target,'gain')
-tree_gain_ratio = train(df_train,target,'gain_ratio')
+    set_init = SetCases(df,len(df),target,classValues)
+    root_node = Node(set_init, 0)
+    decisionTree(set_init, set_init.get_attributes(),root_node,0,classValues)
+    """
+    tree_gain = train(df_train, target,threshold,'gain',separator)
+    tree_gain_ratio = train(df_train,target,threshold,'gain_ratio',separator)
 
-#tree_gain_ratio.printTree(0, graph, tree_gain_ratio,name_counter)
+    #tree_gain_ratio.printTree(0, graph, tree_gain_ratio,name_counter)
 
-#graph.view()
+    #graph.view()
 
-df_test['test_result_gain'] = predict_cases(df_test,tree_gain)
-df_test['correct_prediction_gain'] = df_test[['test_result_gain',target]].apply(lambda x: 1 if x['test_result_gain'] == x[target] else 0, axis=1)
-
+    df_test['test_result_gain'] = predict_cases(df_test,tree_gain)
+    df_test['correct_prediction_gain'] = df_test[['test_result_gain',target]].apply(lambda x: 1 if x['test_result_gain'] == x[target] else 0, axis=1)
 
 
-df_test['test_result_gain_ratio'] = predict_cases(df_test,tree_gain_ratio)
-df_test['correct_prediction_gain_ratio'] = df_test[['test_result_gain_ratio',target]].apply(lambda x: 1 if x['test_result_gain_ratio'] == x[target] else 0, axis=1)
 
-print('---------------------GAIN--------------------------------')
-print('Accuracy',df_test[df_test['correct_prediction_gain']==1]['correct_prediction_gain'].count()/len(df_test))
-print(pd.crosstab(df_test[target],df_test['test_result_gain']))
+    df_test['test_result_gain_ratio'] = predict_cases(df_test,tree_gain_ratio)
+    df_test['correct_prediction_gain_ratio'] = df_test[['test_result_gain_ratio',target]].apply(lambda x: 1 if x['test_result_gain_ratio'] == x[target] else 0, axis=1)
 
-print('---------------------GAIN RATIO--------------------------------')
-print('Accuracy',df_test[df_test['correct_prediction_gain_ratio']==1]['correct_prediction_gain_ratio'].count()/len(df_test))
-print(pd.crosstab(df_test[target],df_test['test_result_gain_ratio']))
+    print('---------------------GAIN--------------------------------')
+    print('Accuracy',df_test[df_test['correct_prediction_gain']==1]['correct_prediction_gain'].count()/len(df_test))
+    print(pd.crosstab(df_test[target],df_test['test_result_gain']))
+
+    print('---------------------GAIN RATIO--------------------------------')
+    print('Accuracy',df_test[df_test['correct_prediction_gain_ratio']==1]['correct_prediction_gain_ratio'].count()/len(df_test))
+    print(pd.crosstab(df_test[target],df_test['test_result_gain_ratio']))
 
 
